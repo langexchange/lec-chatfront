cb0cdac049d1bc3be93adb1d0d8fed9e6b2d8d4b
vutl <vu.tran10100305@gmail.com>
2023-05-22 14:49:15 +0700

    render correct text with country flag and scoreboard

--- src/plugins/langex/chatbot/index.js
+++ src/plugins/langex/chatbot/index.js
@@ -29,14 +29,43 @@ converse.plugins.add('langex-audio-toolkit', {
       // ```xml
       //   <pronunc_assess xmlns="langex:chatbot:pronunc_assess"> </pronunc_assess>
       // ```
+    function processPronuncAssessRichText(msgDoc){
+      // Include flag
+      const languageEl = msgDoc.querySelector("assess-language");
+      const country_iso = languageEl.getAttribute("country_code");
+      if (country_iso){
+        const iconEl = document.createElement("span");
+        iconEl.className = `fi fi-${country_iso} fis`;
+        languageEl.append(iconEl);
+      }
+
+      // Transform color for score
+      const assesedColors = [[30, "#AB121C"], [60, "#0097DA"], [100, "#00692F"]]
+      const scoreEl = msgDoc.querySelector("langex-chatbot-para score");
+      const score = parseInt(scoreEl.textContent);
+
+      let chosenColor = "#AB121C";
+      for (let assessedColor of assesedColors){
+        if(score <= assessedColor[0]){
+          chosenColor = assessedColor[1];
+          break;
+        }
+      }
+      scoreEl.style.backgroundColor = chosenColor;
+    }
+
     function transformAudioBotMsg(richText, options) {
       if (!richText.is_langexbot) return;
-
       console.log("Audio bot message transformation")
       console.log(richText)
-
       const langex_chatbotmsg = document.createElement("langex-chatbotmsg");
       langex_chatbotmsg.innerHTML = richText.toString();
+      
+      if (richText.is_pronunc_assess){
+        processPronuncAssessRichText(langex_chatbotmsg);
+      }
+
+      console.log(langex_chatbotmsg)
       richText.addTemplateResult(0, richText.length, html`${langex_chatbotmsg}`);
     }
 
